a45460bc8bc6c3f511155c41204735ffa3191f6b
MaleeshaDiluminda <maleeshadiluminda@gmail.com>
2023-11-21 15:28:53 +0530

    Mongoose CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,119 @@
 import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
+import { Book } from "./models/bookModels.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome Maleesha Lionel')
 });
 
-console.log('Before mongoose connect');
+//  Route for save a new book
+app.post('/books', async(request, response) =>{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+// Route for get all books from the database
+app.get('/books', async(request, response) => {
+    try{
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for get one book from the database by id
+app.get('/books/:id', async(request, response) => {
+    try{
+        const {id} = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for update a book
+app.put('/books/:id', async(request, response) => {
+    try{
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const {id} = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result){
+            return response.status(404).json({message: 'Book not found'});
+        }
+        return response.status(200).send({message: 'Book updated successfully'})
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+
+// Route for delete a book
+app.delete('/books/:id', async(request, response) => {
+    try{
+        const {id} = request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message: 'Book not found'})
+        }
+
+        return response.status(200).send({message: 'Book deleted successfully'})
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+
 
 mongoose 
    .connect(mongoDBURL)
    .then(() => {
-    // console.log('App connected to databse');
+    console.log('App connected to databse');
     app.listen(PORT, () => {
         console.log(`App is listening to port: ${PORT}`);
     });
@@ -22,4 +121,3 @@ mongoose
    .catch((error) => {
     console.log('Error connecting to database:',error);
    });
-//    console.log('After mongoose connect'); 
